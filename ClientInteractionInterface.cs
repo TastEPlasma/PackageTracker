2b483f4603ea1fbc5a05fc07d9afb452c7e450aa
ER Webster <TastEPlasma@gmail.com>
2015-10-01 21:47:12 -0500

    Initial Upload
    Contains basic concepts and behaviors in commented form.

---
+++ ClientInteractionInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageTracker
+{
+    interface IClientInteraction
+    {
+        //publish events when client interacts with display
+        //request updates to display
+        //Start/Exit Program
+
+    }
+}
---
+++ LocalDataInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageTracker
+{
+    interface ILocalDataAccess
+    {
+        //Open local SQL connection
+        //Read from connection
+        //Write to connection
+        //Request data updates from WebAccess
+    }
+}
